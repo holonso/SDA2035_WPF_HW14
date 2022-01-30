c91617dfa702dfd0d50ec97f0b5a7fa4de4571a1
holonso <d1mo@live.com>
2022-01-30 16:39:56 +0900

    Добавьте файлы проекта.

---
+++ SDA2035_WPF_HW14/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SDA2035_WPF_HW14
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>();
+            products.Add(new Product()
+            {
+                NameProduct = "Хлеб",
+                CostProduct = 55,
+                TypeProduct = IEnumTypeProduct.Food,
+            });
+            products.Add(new Product()
+            {
+                NameProduct = "Микроволновая печь",
+                CostProduct = 4760,
+                TypeProduct = IEnumTypeProduct.Tech,
+            });
+            products.Add(new Product()
+            {
+                NameProduct = "Молоко",
+                CostProduct = 82,
+                TypeProduct = IEnumTypeProduct.Food,
+            });
+            lst.ItemsSource = products;
+        }
+    }
+}
---
+++ SDA2035_WPF_HW14/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDA2035_WPF_HW14
+{
+    public enum IEnumTypeProduct
+    {
+        Food,
+        Tech
+    }
+    public class Product
+    {
+        public string NameProduct { get; set; }
+        public double CostProduct { get; set; }
+        public IEnumTypeProduct TypeProduct { get; set; }  
+    }
+}
